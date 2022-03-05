0ae3ce4969a06bd4e4af89b8f6f6d9b95baf4950
ArneWaumans <arne.waumans2005@gmail.com>
2022-03-05 18:42:02 +0100

    made the program build ready

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,11 @@
 from pprint import pprint
 
-from parseconf import ParsePatches
 from getpatchnames import GetPatchNames
 from disectpatches import CreatePatchFiles
 from applypatches import ApplyPatches
 
 def main():
-    patchlist = [list(patch.values())[0] for patch in ParsePatches("config.json")['patches']]
-    patchnames = GetPatchNames(patchlist)
+    patchnames = GetPatchNames()
     CreatePatchFiles(patchnames)
     ApplyPatches()
 
